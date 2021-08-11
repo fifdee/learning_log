1d03fb07af3e58894eecd082c5d12956a9b92431
fifdee <fifdee@outlook.com>
2021-08-11 10:50:59 +0200

    2nd

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'askjdhasdk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # my apps:
     'learning_logs',
+    'users',
 
     # default apps:
     'django.contrib.admin',
